3e8bd082bebf920fb1c3cebf8c8a30e13c359876
Kevin-Payan <kevinpn@hotmail.com>
2024-11-05 15:42:05 -0800

    coming soon placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="flex justify-center w-screen h-screen items-center bg-black">
+    <h1 class="text-4xl font-bold text-white">coming soon.</h1>
+  </div>
 </template>
